7754c328979a9813f8a8d451d8ee518786391341
myyrakle <sssang97@naver.com>
2026-05-27 03:03:45 +0900

    echov4 사용성 개선

--- echov4/serve.go
+++ echov4/serve.go
@@ -8,43 +8,74 @@ import (
 	swagger "github.com/myyrakle/go-swagger-ui"
 )
 
-func Setup(e *echo.Echo, prefix string) {
+type SpecFormat string
+
+const (
+	JSON SpecFormat = "json"
+	YAML SpecFormat = "yaml"
+)
+
+type SpecSource interface {
+	Format() SpecFormat
+	Content() []byte
 }
 
-// Handler는 임베드된 Swagger UI 리소스를 서빙하는 echo 핸들러입니다.
-//
-// `/swagger/*` 와 같이 catch-all 라우트로 등록하여 사용합니다.
-// 처리 경로:
-//   - "" (트레일링 슬래시 접근) → index.html 로 리다이렉트
-//   - index.html               → 임베드된 엔트리 HTML
-//   - swagger-ui.css, index.css → 임베드된 CSS
-//   - swagger-ui-bundle.js, swagger-ui-standalone-preset.js → 임베드된 JS
-//
-// index.html은 ./doc.json 상대 경로로 스펙을 로드하므로,
-// 같은 prefix 하위에 doc.json 응답 핸들러를 별도로 등록해야 합니다.
-func Handler(c echo.Context) error {
-	file := c.Param("*")
-	if file == "" {
-		base := strings.TrimSuffix(c.Request().URL.Path, "/")
-		return c.Redirect(http.StatusMovedPermanently, base+"/index.html")
-	}
+type SpecSourceJSON struct {
+	JSON []byte
+}
 
-	switch file {
-	case "index.html":
-		return c.HTMLBlob(http.StatusOK, []byte(swagger.IndexHTML))
-	case "swagger-ui.css":
-		return c.Blob(http.StatusOK, "text/css; charset=utf-8", []byte(swagger.SwaggerUICSS))
-	case "index.css":
-		return c.Blob(http.StatusOK, "text/css; charset=utf-8", []byte(swagger.IndexCSS))
-	case "swagger-ui-bundle.js":
-		return c.Blob(http.StatusOK, "application/javascript; charset=utf-8", []byte(swagger.SwaggerUIBundleJS))
-	case "swagger-ui-standalone-preset.js":
-		return c.Blob(http.StatusOK, "application/javascript; charset=utf-8", []byte(swagger.SwaggerUIStandalonePresetJS))
-	case "favicon-16x16.png":
-		return c.Blob(http.StatusOK, "image/png", swagger.Favicon16PNG)
-	case "favicon-32x32.png":
-		return c.Blob(http.StatusOK, "image/png", swagger.Favicon32PNG)
-	}
+func (s SpecSourceJSON) Format() SpecFormat {
+	return JSON
+}
 
-	return echo.ErrNotFound
+func (s SpecSourceJSON) Content() []byte {
+	return s.JSON
+}
+
+type SpecSourceYAML struct {
+	YAML []byte
+}
+
+func (s SpecSourceYAML) Format() SpecFormat {
+	return YAML
+}
+
+func (s SpecSourceYAML) Content() []byte {
+	return s.YAML
+}
+
+func Setup(e *echo.Echo, prefix string, format SpecFormat) {
+	e.GET(prefix+"*",
+		func(c echo.Context) error {
+			file := c.Param("*")
+			if file == "" {
+				base := strings.TrimSuffix(c.Request().URL.Path, "/")
+				return c.Redirect(http.StatusMovedPermanently, base+"/index.html")
+			}
+
+			switch file {
+			case "index.html":
+				switch format {
+				case JSON:
+					return c.HTMLBlob(http.StatusOK, []byte(swagger.JSONIndexHTML))
+				case YAML:
+					return c.HTMLBlob(http.StatusOK, []byte(swagger.YAMLIndexHTML))
+				}
+			case "swagger-ui.css":
+				return c.Blob(http.StatusOK, "text/css; charset=utf-8", []byte(swagger.SwaggerUICSS))
+			case "index.css":
+				return c.Blob(http.StatusOK, "text/css; charset=utf-8", []byte(swagger.IndexCSS))
+			case "swagger-ui-bundle.js":
+				return c.Blob(http.StatusOK, "application/javascript; charset=utf-8", []byte(swagger.SwaggerUIBundleJS))
+			case "swagger-ui-standalone-preset.js":
+				return c.Blob(http.StatusOK, "application/javascript; charset=utf-8", []byte(swagger.SwaggerUIStandalonePresetJS))
+			case "favicon-16x16.png":
+				return c.Blob(http.StatusOK, "image/png", swagger.Favicon16PNG)
+			case "favicon-32x32.png":
+				return c.Blob(http.StatusOK, "image/png", swagger.Favicon32PNG)
+			}
+
+			return echo.ErrNotFound
+		},
+	)
 }
